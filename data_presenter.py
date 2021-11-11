a60dde92f42110e0bd43bb86e3b5dffb591cb0cc
jaylen  hall <jaylenhall1@gmail.com>
2021-11-11 16:29:57 -0600

    done for  the day

---
+++ data_presenter.py
@@ -0,0 +1,23 @@
+open_file = open("CupcakeInvoices.csv")
+
+
+# for line in open_file:
+#     print(line)
+# for line in open_file:
+#     values = line.split(",")
+#     print(values[2])
+
+# for  line in open_file:
+#     values = line.split(',')
+#     total = int(values[3]) * float(values[4])
+#     print(total)
+
+
+# totals = 0
+
+# for line in open_file:
+#     values = line.split(',')
+#     totals = totals + (int(values[3]) * float(values[4]))
+# print(totals)
+
+open_file.close()
